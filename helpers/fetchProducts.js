2931a31957b4be7009247a18061c73fe08ca00e4
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-02-11 17:50:34 -0500

    Requesito 8, cinco fucoes emplementadas

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,12 +1,15 @@
 const fetchProducts = async (id) => {
   // seu código aqui
-  const produto = id;
-  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return err;
+  }
 };
-
+fetchProducts('computador');
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,28 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  it('1 - Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function')
+  });
+  it('2 - Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('3 - Teste a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/sites/MLB/search?q=computador")
+  });
+  it('4 - Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+    const resultErr = new Error('You must provide an url');
+    try {
+      await fetchProducts();
+    } catch (err){
+      expect(err).toEqual(resultErr);
+    }
+  });
 });
