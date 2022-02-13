634a2c4a04c7dad00ac10f611b5710e2daea5ea2
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-02-12 21:44:54 -0500

    Q.10, resolvida. Implementado os Teste.

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,14 @@ localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  const teste = "<ol><li>Item</li></ol>";
+  it('1 - Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+    saveCartItems(teste)
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('2 - Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    saveCartItems(teste)
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', teste)
+  });
 });
